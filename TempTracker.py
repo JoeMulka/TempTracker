a7cfbdefae6da738c4af0c312f40b85309be38ef
Joseph Mulka <mulkajos@msu.edu>
2019-03-16 23:44:00 -0400

    testing methods of temp read

--- TempTracker.py
+++ TempTracker.py
@@ -20,7 +20,15 @@ def celsiusToFahrenheit(cels):
 temp = sense.get_temperature()
 temp = celsiusToFahrenheit(temp)
 humidity = sense.get_humidity()
-print (current_time + " " + '%.2f'%(temp) + " " + '%.2f'%(humidity))
+# Testing to see if the other ways to get temperature are more accurate
+humidity_temp = sense.get_temperature_from_humidity()
+humidity_temp = celsiusToFahrenheit(humidity_temp)
+pressure_temp = sense.get_temperature_from_pressure()
+pressure_temp = celsiusToFahrenheit(pressure_temp)
+
+print (current_time + " " + '%.2f'%(temp) + "F " + '%.2f'%(humidity)+'%')
+print("humidity temp is: " + str(humidity_temp))
+print("pressure temp is: " + str(pressure_temp))
 
 
 # This shows the proper way to write the csv so that it will open in excel/libreoffice in two columns
